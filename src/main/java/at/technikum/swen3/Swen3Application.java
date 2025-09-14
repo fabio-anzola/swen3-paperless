1052571fc1f56e828a6e6db823a1ad3a99ba3c4b
Anzola Fabio <anzola.fabio@gmail.com>
2025-09-14 18:52:32 +0200

    added path for entity and repository

--- src/main/java/at/technikum/swen3/Swen3Application.java
+++ src/main/java/at/technikum/swen3/Swen3Application.java
@@ -2,12 +2,19 @@ package at.technikum.swen3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "at.technikum.swen3")
+@EnableJpaRepositories(basePackages = "at.technikum.swen3.repository")
+@EntityScan(basePackages = "at.technikum.swen3.entity")
 public class Swen3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Swen3Application.class, args);
 	}
-
 }
