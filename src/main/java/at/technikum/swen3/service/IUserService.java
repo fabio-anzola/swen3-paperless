1067ecdaa64065b480351f272a17376a5d2bc9e5
Anzola Fabio <anzola.fabio@gmail.com>
2025-09-14 22:39:47 +0200

    changed naming convention of service interface

---
+++ src/main/java/at/technikum/swen3/service/IUserService.java
@@ -0,0 +1,8 @@
+package at.technikum.swen3.service;
+
+import at.technikum.swen3.entity.User;
+
+public interface IUserService {
+  User registerUser(User user);
+  User deleteUser(Long id);
+}
--- src/main/java/at/technikum/swen3/service/UserService.java
+++ src/main/java/at/technikum/swen3/service/UserService.java
@@ -1,8 +1,32 @@
 package at.technikum.swen3.service;
 
 import at.technikum.swen3.entity.User;
+import at.technikum.swen3.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface UserService {
-  User registerUser(User user);
-  User deleteUser(Long id);
+@Service
+public class UserService implements IUserService {
+  private final UserRepository userRepository;
+
+  @Autowired
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public User registerUser(User user) {
+   return userRepository.save(user);
+  }
+
+  @Override
+  @Transactional
+  public User deleteUser(Long id) {
+    User existing = userRepository.findById(id)
+            .orElseThrow(() -> new EntityNotFoundException("User " + id + " not found"));
+    userRepository.delete(existing);
+    return existing;
+  }
 }
