225ac2db64f53a1c980a91f4b72914ba1bf49578
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2025-09-14 20:52:01 +0200

    made db connection work on windows by changing host port

--- src/main/java/at/technikum/swen3/Swen3Application.java
+++ src/main/java/at/technikum/swen3/Swen3Application.java
@@ -10,9 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "at.technikum.swen3")
-@EnableJpaRepositories(basePackages = "at.technikum.swen3.repository")
-@EntityScan(basePackages = "at.technikum.swen3.entity")
+@SpringBootApplication
 public class Swen3Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Swen3Application.class, args);
