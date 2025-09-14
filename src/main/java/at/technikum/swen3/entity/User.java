3ac64aee14e829606d11e26ab52bcc228f87569d
Leonhard Kenshi Müllauer <52050699+Reonharudo@users.noreply.github.com>
2025-09-14 22:35:32 +0200

    Update User.java

--- src/main/java/at/technikum/swen3/entity/User.java
+++ src/main/java/at/technikum/swen3/entity/User.java
@@ -13,7 +13,7 @@ import lombok.*;
 @Table(name = "users")
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @NotNull
