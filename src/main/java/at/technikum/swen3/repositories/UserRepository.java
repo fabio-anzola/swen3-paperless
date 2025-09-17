d6983ba3b08cd593416ec64334e4408f157d6530
Anzola Fabio <anzola.fabio@gmail.com>
2025-09-17 18:20:23 +0200

    removed unused import

--- src/main/java/at/technikum/swen3/repositories/UserRepository.java
+++ src/main/java/at/technikum/swen3/repositories/UserRepository.java
@@ -4,7 +4,5 @@ import at.technikum.swen3.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> { }
