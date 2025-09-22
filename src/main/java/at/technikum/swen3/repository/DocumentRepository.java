8daf88d114759a606e2a71b61973a4667ed001ac
Anzola Fabio <anzola.fabio@gmail.com>
2025-09-22 12:08:45 +0200

    added doc repo

---
+++ src/main/java/at/technikum/swen3/repository/DocumentRepository.java
@@ -0,0 +1,13 @@
+package at.technikum.swen3.repository;
+
+import at.technikum.swen3.entity.Document;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    Page<Document> findAllByOwner_Id(Long ownerId, Pageable pageable);
+    boolean existsByIdAndOwner_Id(Long id, Long ownerId);
+}
