bf4a5bc0c07a6df150d335db948471b45e0acbb4
Anzola Fabio <anzola.fabio@gmail.com>
2025-09-29 19:52:25 +0200

    removed print

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
         ? process.env.BACKEND_URL || "http://app:8080"
         : "http://localhost:8080";
 
-    console.log(`Using backend URL: ${backend}`);
+    //console.log(`Using backend URL: ${backend}`);
 
     return [{ source: "/api/:path*", destination: `${backend}/api/:path*` }];
   },
