0826f5208debf8f38cc47dfef3a1bc63c8ec0e44
Anzola Fabio <anzola.fabio@gmail.com>
2025-10-02 15:33:27 +0200

    added basic kafka service

---
+++ apps/rest/src/main/java/at/technikum/swen3/kafka/KafkaController.java
@@ -0,0 +1,23 @@
+package at.technikum.swen3.kafka;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class KafkaController {
+
+    private final KafkaProducerService producerService;
+
+    public KafkaController(KafkaProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping("/publish")
+    public String sendMessageToKafka(@RequestParam("message") String message) {
+        producerService.sendMessage("my-topic", message);
+        return "Message sent to Kafka: " + message;
+    }
+}
