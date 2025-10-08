e2fa33ec6a9a7fd502498872df9914b1f3c1a022
Anzola Fabio <anzola.fabio@gmail.com>
2025-10-08 17:52:30 +0200

    added kafka service

---
+++ apps/rest/src/main/java/at/technikum/swen3/kafka/KafkaProducerService.java
@@ -0,0 +1,19 @@
+package at.technikum.swen3.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic, String message) {
+        kafkaTemplate.send(topic, message);
+        System.out.println("Message sent to topic " + topic + ": " + message);
+    }
+}
