95050ee987ffa5e653badf66fa28050abf523910
Gr-eggs <165694869+Gr-eggs@users.noreply.github.com>
2025-10-08 22:47:43 +0200

    added user validation

--- apps/rest/src/main/java/at/technikum/swen3/service/dtos/user/UserDto.java
+++ apps/rest/src/main/java/at/technikum/swen3/service/dtos/user/UserDto.java
@@ -1,4 +1,10 @@
 package at.technikum.swen3.service.dtos.user;
 
-public record UserDto(String username) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UserDto(
+        @NotNull @NotBlank @Size(min = 3, max = 50)
+        String username
+) {}
